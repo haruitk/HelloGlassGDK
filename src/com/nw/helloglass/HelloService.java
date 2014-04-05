3a1bd9b6ae37e47d5b3588f4a1b42fcb087ad0f8
Harit Srivastava <harit.srivastava@gmail.com>
2014-04-05 13:37:20 -0400

    project files

---
+++ src/com/nw/helloglass/HelloService.java
@@ -0,0 +1,67 @@
+package com.nw.helloglass;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.LiveCard.PublishMode;
+import com.google.android.glass.timeline.TimelineManager;
+
+public class HelloService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	private static final String CARD_TAG = "helloGlass";
+	private HelloDrawer mHelloDrawer;
+
+    private TimelineManager mTimelineManager;
+    private LiveCard mLiveCard;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mTimelineManager = TimelineManager.from(this);
+        mHelloDrawer = new HelloDrawer(this);
+    }
+       
+
+    
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+    	
+    	 if (mLiveCard == null) {
+             mLiveCard = mTimelineManager.createLiveCard(CARD_TAG);
+             Intent mainIntent = new Intent(this, MainActivity.class);
+             mLiveCard.setDirectRenderingEnabled(true).getSurfaceHolder().addCallback(mHelloDrawer);
+             
+             mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+             mLiveCard.setAction(PendingIntent.getActivity(this, 0, mainIntent, 0));
+             mLiveCard.publish(PublishMode.REVEAL);
+         } else {
+             // TODO(alainv): Jump to the LiveCard when API is available.
+         }
+       
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+    	
+    	if (mLiveCard != null && mLiveCard.isPublished()) {
+            mLiveCard.unpublish();
+            mLiveCard = null;
+        }
+        
+        super.onDestroy();
+    }
+
+}
